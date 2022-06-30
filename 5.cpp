a41fd95cfd4a10210996f13ee286fb2c18da006b
Ashiq Uddin Pranto <ashiqpranto@gmail.com>
2022-06-30 10:05:25 +0600

    5 done

--- 5.cpp
+++ 5.cpp
@@ -1,12 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isCharacter(char c)
+{
+    if((c>='a' && c<='z') || (c>='A' && c<='Z'))
+    {
+        return true;
+    }
+    return false;
+}
+
 bool isOther(string str)
 {
     int sz = str.size();
     for(int i = 0;i<sz;i++)
     {
-        if(isdigit(str[i]) || isCharacter(str[i]))
+        if(isdigit(str[i]) || isCharacter(str[i]) || str[i]=='.')
         {
             continue;
         }else{
@@ -16,6 +25,27 @@ bool isOther(string str)
     return false;
 }
 
+int isFloat(string str)
+{
+    int sz = str.size();
+    int i = 0;
+    while(i<sz && str[i]!='.')
+    {
+        if(str[i]<'0' && str[i]>'9') return 0;
+        i++;
+    }
+    i++;
+    int cnt = 0;
+    for(;i<sz;i++)
+    {
+        if(str[i]<'0' && str[i]>'9') return 0;
+        cnt++;
+    }
+    return cnt;
+}
+
+
+
 int main()
 {
     freopen("input.txt","r",stdin);
@@ -23,6 +53,27 @@ int main()
     string str;
     getline(cin,str);
 
+    int ln = str.size();
+
+    if(isOther(str))
+    {
+        cout<<"Invalid Input or Undefined\n";
+    }
+    else if((str[0]>='a' && str[0]<='h') || (str[0]>='A' && str[0]<='H') || (str[0]>='o' && str[0]<='z') || (str[0]>='O' && str[0]<='Z'))
+    {
+        cout<<"Float Variable\n";
+    }
+    else if((isFloat(str)==2 || isFloat(str)==1) && ((str[0]>='0' && str[0]<='9') || str[0]=='.'))
+    {
+        cout<<"Float Number\n";
+    }
+    else if(isFloat(str)>2 && ((str[0]>='0' && str[0]<='9') || str[0]=='.'))
+    {
+        cout<<"Double Number\n";
+    }
+    else{
+        cout<<"Invalid Input or Undefined else\n";
+    }
     
 
 }
